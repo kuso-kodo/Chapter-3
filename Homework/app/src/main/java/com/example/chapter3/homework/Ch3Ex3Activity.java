9592f15af5ab9bb462168a9f96c0535aa39689f1
name1e5s <name1e5s@qq.com>
2020-03-31 19:15:27 +0800

    Ex3

--- Homework/app/src/main/java/com/example/chapter3/homework/Ch3Ex3Activity.java
+++ Homework/app/src/main/java/com/example/chapter3/homework/Ch3Ex3Activity.java
@@ -1,5 +1,10 @@
 package com.example.chapter3.homework;
 
+import android.support.annotation.Nullable;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -16,12 +21,28 @@ public class Ch3Ex3Activity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ch3ex3);
 
+        ViewPager viewPager = findViewById(R.id.view_pager);
+        viewPager.setAdapter(new FragmentPagerAdapter(getSupportFragmentManager()) {
+            @Override
+            public Fragment getItem(int i) {
+                return new PlaceholderFragment();
+            }
 
+            @Override
+            public int getCount() {
+                return 2;
+            }
 
-        // TODO: ex3-1. 添加 ViewPager 和 Fragment 做可滑动界面
+            @Override
+            public CharSequence getPageTitle(int position) {
+                if(position == 0) {
+                    return "好友列表";
+                }
+                return "我的好友";
+            }
+        });
 
-
-
-        // TODO: ex3-2, 添加 TabLayout 支持 Tab
+        TabLayout tabLayout = findViewById(R.id.tab_layout);
+        tabLayout.setupWithViewPager(viewPager);
     }
 }
--- Homework/app/src/main/java/com/example/chapter3/homework/PlaceholderFragment.java
+++ Homework/app/src/main/java/com/example/chapter3/homework/PlaceholderFragment.java
@@ -1,15 +1,36 @@
 package com.example.chapter3.homework;
 
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Adapter;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+import android.widget.TextView;
+
+import com.airbnb.lottie.LottieAnimationView;
+
+import java.util.List;
+import java.util.Objects;
 
 public class PlaceholderFragment extends Fragment {
-
+    static String[] friendList = {
+            "带带大师兄",
+            "川建国",
+            "奥观海",
+            "明仁天皇",
+            "YYUT",
+            "高槻律" };
+    
+    ListView listView;
+    LottieAnimationView animationView;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -21,12 +42,31 @@ public class PlaceholderFragment extends Fragment {
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
-
+        listView = Objects.requireNonNull(getView()).findViewById(R.id.list_view);
+        ArrayAdapter<String> itemsAdapter =
+                new ArrayAdapter<String>(Objects.requireNonNull(getActivity()),
+                        android.R.layout.simple_list_item_1, friendList);
+        listView.setAdapter(itemsAdapter);
+        animationView = getView().findViewById(R.id.animation_view_ex3);
         getView().postDelayed(new Runnable() {
             @Override
             public void run() {
-                // 这里会在 5s 后执行
-                // TODO ex3-4：实现动画，将 lottie 控件淡出，列表数据淡入
+                listView.setVisibility(View.VISIBLE);
+                listView.setAlpha(0f);
+                listView.animate()
+                        .alpha(1f)
+                        .setDuration(500)
+                        .setListener(null);
+
+                animationView.animate()
+                        .alpha(0f)
+                        .setDuration(500)
+                        .setListener(new AnimatorListenerAdapter() {
+                            @Override
+                            public void onAnimationEnd(Animator animation) {
+                                animationView.setVisibility(View.GONE);
+                            }
+                        });
             }
         }, 5000);
     }
